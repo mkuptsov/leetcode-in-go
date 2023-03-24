407343758603bf62f2571ef33d13487915bb21c9
Maxim Kuptsov <maksim-kuptsov@yandex.ru>
2023-03-24 14:32:14 +0300

    problem 2235 solution is added

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/mkuptsov/leetcode-in-go/problem2235"
+)
 
 func main() {
-	fmt.Println("Hello, world")
+	fmt.Println(problem2235.Sum(2, 3))
 }
---
+++ problem2235/2235.go
@@ -0,0 +1,6 @@
+package problem2235
+
+func Sum(num1 int, num2 int) int {
+    sum := num1 + num2
+    return sum
+}
