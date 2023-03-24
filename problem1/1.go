88d1efe65063e92f96443d9c0169c6b770ed987f
Maxim Kuptsov <maksim-kuptsov@yandex.ru>
2023-03-24 18:59:26 +0300

    problem1 solution and test have been added

---
+++ problem1/1.go
@@ -0,0 +1,13 @@
+package problem1
+
+func TwoSum(nums []int, target int) []int {
+    var index1, index2 int
+    for i := 0; i < len(nums) - 1; i++ {
+        for j := 1; j < len(nums); j++ {
+            if nums[i] + nums[j] == target {
+                index1, index2 = i, j
+            }
+        }
+    }
+    return []int{index1, index2}
+}
