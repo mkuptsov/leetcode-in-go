21b1e6cb6b88246a1659b63ca3b11b85d8f6b85c
Maxim Kuptsov <maksim-kuptsov@yandex.ru>
2023-03-25 00:34:15 +0300

    problem1 has been corrected

--- problem1/1.go
+++ problem1/1.go
@@ -3,7 +3,7 @@ package problem1
 func TwoSum(nums []int, target int) []int {
     var index1, index2 int
     for i := 0; i < len(nums) - 1; i++ {
-        for j := 1; j < len(nums); j++ {
+        for j := i + 1; j < len(nums); j++ {
             if nums[i] + nums[j] == target {
                 index1, index2 = i, j
             }
