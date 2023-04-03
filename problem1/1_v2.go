e5ac1a4f903dbb4885cb2788fd7523a3cb127595
Maxim Kuptsov <maksim-kuptsov@yandex.ru>
2023-04-03 09:30:48 +0300

    problem 1 v2 has been corrected

--- problem1/1_v2.go
+++ problem1/1_v2.go
@@ -1,13 +1,15 @@
+package problem1
+
 func twoSum(nums []int, target int) []int {
-    numsMap := map[int]int{}
-    var result []int
-    for i := 0; i < len(nums); i++ {
-        if _, ok := numsMap[target - nums[i]]; ok {
-            result = []int{numsMap[target - nums[i]], i}
-            break
-        } else {
-            numsMap[nums[i]] = i
-        }
-    }
-    return result
+	numsMap := map[int]int{}
+	var result []int
+	for i := 0; i < len(nums); i++ {
+		if _, ok := numsMap[target-nums[i]]; ok {
+			result = []int{numsMap[target-nums[i]], i}
+			break
+		} else {
+			numsMap[nums[i]] = i
+		}
+	}
+	return result
 }
