d5ff1de1aa9c414ecc61f7ce27c7674d7647ced8
Maxim Kuptsov <maksim-kuptsov@yandex.ru>
2023-04-03 09:25:19 +0300

    problem 1290

---
+++ problem1290/1290.go
@@ -0,0 +1,19 @@
+package problem1290
+
+import "github.com/mkuptsov/leetcode-in-go/utils"
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getDecimalValue(head *utils.ListNode) int {
+	decNum := head.Val
+	for node := head; node.Next != nil; node = node.Next {
+		decNum = decNum*2 + node.Next.Val
+	}
+	return decNum
+
+}
