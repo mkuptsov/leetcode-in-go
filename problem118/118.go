3e3b589059f110a6b14787775ad4eda0c7243748
Maxim Kuptsov <maksim-kuptsov@yandex.ru>
2023-04-15 02:28:08 +0300

    problem 118

---
+++ problem118/118.go
@@ -0,0 +1,14 @@
+package problem118
+
+func generate(numRows int) [][]int {
+	triangle := make([][]int, numRows)
+
+	for i := 0; i < numRows; i++ {
+		triangle[i] = make([]int, i+1)
+		triangle[i][0], triangle[i][i] = 1, 1
+		for j := 1; j < i; j++ {
+			triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+		}
+	}
+	return triangle
+}
