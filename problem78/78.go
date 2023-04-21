05987509f5ed89c25dcf1a73e5a3ebab154612b0
Maxim Kuptsov <maksim-kuptsov@yandex.ru>
2023-04-21 08:44:15 +0300

    problem 78. Subsets

---
+++ problem78/78.go
@@ -0,0 +1,24 @@
+package problem78
+
+func subsets(nums []int) [][]int {
+	result := [][]int{{}}
+	buf := []int{}
+
+	var backtrack func(int, int, []int)
+	backtrack = func(start, left int, cur []int) {
+		if left == 0 {
+			return
+		}
+
+		for i := start; i < len(nums); i++ {
+			buf = append(buf, cur[i])
+			copied := make([]int, len(buf))
+			copy(copied, buf)
+			result = append(result, copied)
+			backtrack(i+1, left-1, cur)
+			buf = buf[:len(buf)-1]
+		}
+	}
+	backtrack(0, len(nums), nums)
+	return result
+}
